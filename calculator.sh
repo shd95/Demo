86ecc3219dc4796fcf934f9000bdcb00152856e5
shd95 <sriharshadevale@gmail.com>
2021-03-29 19:50:06 +0530

    added add, subtract, multiply and division logic

--- calculator.sh
+++ calculator.sh
@@ -3,3 +3,13 @@ number1=2;
 number2=5;
 add=$(($number1+$number2));
 echo "$add";
+
+sub=$(($number1-$number2));
+echo "$sub";
+
+mult=$(($number1*$number2));
+echo "$mult";
+
+div=$(($number1/$number2));
+echo "$div";
+
